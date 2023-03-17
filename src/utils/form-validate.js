9fd5a76a182d8dad3fcc13f6ed1147d294b0f3d8
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-18 00:39:35 +0530

    Form Validation

---
+++ src/utils/form-validate.js
@@ -0,0 +1,20 @@
+export const userNameValidate = {
+    required : {
+        value : true,
+        message : 'Please Enter your Username',
+    }
+}
+
+export const emailValidate = {
+    required : {
+        value : true,
+        message : 'Please Enter your email ID'
+    }
+}
+
+export const passwordValidate = {
+    required : {
+        value : true,
+        message : 'Please Enter your password'
+    }
+}
