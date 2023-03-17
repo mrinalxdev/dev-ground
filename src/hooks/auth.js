d51f1215d74e2aa5e96bade3a6a747dde7b15c56
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-18 00:23:05 +0530

    feat: Toast Function from Chakra UI

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -3,6 +3,8 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../lib/firebase'
 import { useState } from 'react'
 import { DASHBOARD } from '../lib/routes'
+import { useToast } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 export const useAuth = () => {
 
@@ -17,13 +19,39 @@ export const useAuth = () => {
 
 export const useLogin = () => {
     const [isLoading, setLoading] = useState(false)
+    const toast = useToast()
+    const navigate = useNavigate()
 
-    const login = async({email, password, redirectedTo=DASHBOARD}) => {
+
+    const login = async({email, password, redirectTo=DASHBOARD}) => {
         setLoading(true)
 
-        await signInWithEmailAndPassword(auth, email, password)
+        try{
+            await signInWithEmailAndPassword(auth, email, password)
+            toast({
+                title:'You are Logged In',
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+                position: "top",
+            })
+            navigate(redirectTo)
+        }catch(error){
+            toast({
+                title: 'Login Failed',
+                status: 'error',
+                description: error.message,
+                position: 'top',
+                duration: 5000,
+                isClosable: true,
+            })
+            return false
+
+        }
+
 
         setLoading(false)
+        return(true)
     }
 
 
