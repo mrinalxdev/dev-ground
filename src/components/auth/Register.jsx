2db898585142c11b6d6138f0bfb30942acdba785
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-19 00:06:12 +0530

    Added Data

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,34 +1,56 @@
 import React from 'react'
-import { Center, Box, Heading, Input, FormLabel, FormControl, FormErrorMessage } from '@chakra-ui/react'
+import { Center, Box, Heading, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
-import { LOGIN } from '../../lib/routes'
+import { useForm } from 'react-hook-form'
+import { useRegister } from '../../hooks/auth'
+import { emailValidate, passwordValidate } from '../../utils/form-validate'
+import { DASHBOARD, LOGIN } from '../../lib/routes'
 
 const Register = () => {
-  return (
-    <Center w="100%" h="100vh">
-      <Box mx="3" maxW="xl" borderWidth="1px" p="10" borderRadius="lg">
-        <Heading mb="4" size="lg" textAlign="center">
-            Register
-        </Heading>
+    const {register : signup, isLoading} = useRegister()
+    const { register, handleSubmit, reset } = useForm()
 
-        <form onSubmit={() => {}}>
+    const handleRegister = async(data) => {
+        const succeded = await signup({
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            redirectTo: DASHBOARD,
+        })
+
+        if(succeded) reset()
+    }
+
+  return (
+    <Center w='100%' h='100vh'>
+        <Box mx="3" maxW="xl" p="9" borderWidth="1px" borderRadius="lg">
+            <Heading mb="4" size="lg" textAlign="center">
+                Register
+            </Heading>
+
+            <form onSubmit={handleSubmit(handleRegister)}>
+                <FormControl isInvalid={false} py="2">
+                    <FormLabel>Username</FormLabel>
+                    <Input type='text' placeholder='Enter Your username'/>
+                    <FormErrorMessage>Invalid username for Authentication </FormErrorMessage>
+                </FormControl>
                 <FormControl isInvalid={false} py="2">
                     <FormLabel>Email</FormLabel>
-                    <Input type='email' placeholder='Enter Your Email' />
+                    <Input type='email' placeholder='Enter Your Email' {...register('email', emailValidate)} />
                     <FormErrorMessage>Invalid email for Authentication </FormErrorMessage>
                 </FormControl>
-                <FormControl isInvalid={false} py="2">
+                <FormControl isInvalid={true} py="2">
                     <FormLabel>Password</FormLabel>
-                    <Input type='password' placeholder='Enter Your password' />
-                    <FormErrorMessage>Invalid email for Authentication </FormErrorMessage>
+                    <Input type='password' placeholder='Enter Your password' {...register('password', passwordValidate)} />
+                    <FormErrorMessage>Invalid password for Authentication </FormErrorMessage>
                 </FormControl>
 
-                <button className="bg-cyan-700 mt-2 px-4 py-2 w-full rounded-lg duration-150 ease-out hover:bg-cyan-400">Login</button>
-        </form>
+                <button className="bg-cyan-700 mt-2 px-4 py-2 w-full rounded-lg duration-150 ease-out hover:bg-cyan-400">Register</button>
+            </form>
 
-        <p className="mt-6"><Link to={LOGIN} className='font-bold text-cyan-900 hover:text-cyan-400'>Login</Link>
-          {" "} Do you have an Account then Login</p>
-      </Box>
+            <p><Link to={LOGIN} className='font-bold text-cyan-900 hover:text-cyan-400'>Login</Link>
+             {" "}  If you have an account </p>
+        </Box>
     </Center>
   )
 }
