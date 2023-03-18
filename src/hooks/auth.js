a927956e5441c24b8e0796abfbf659e2d3a637f7
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-18 10:50:40 +0530

    Feat: Login Functionality Complete

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -3,7 +3,8 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../lib/firebase'
 import { useState } from 'react'
 import { DASHBOARD } from '../lib/routes'
-import { useToast } from '@chakra-ui/react'
+import { useToast} from '@chakra-ui/react'
+// import { Box } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 
 export const useAuth = () => {
@@ -29,21 +30,29 @@ export const useLogin = () => {
         try{
             await signInWithEmailAndPassword(auth, email, password)
             toast({
-                title:'You are Logged In',
-                status: 'success',
                 duration: 5000,
                 isClosable: true,
                 position: "top",
+                // render: () => (
+                //     <Box color='white' p={3} bg='black.500'>
+                //         <h1>Login Successfull</h1>
+                //     </Box>
+                // ),
+                title: 'Login Successfull',
             })
             navigate(redirectTo)
         }catch(error){
             toast({
-                title: 'Login Failed',
-                status: 'error',
                 description: error.message,
                 position: 'top',
-                duration: 5000,
+                duration: 1000,
                 isClosable: true,
+                // render: () => (
+                //     <Box color='red.500' p={3} bg='black.500' borderWidth={4} textAlign='center' borderRadius={2}>
+                //         <h1>Login Failed</h1>
+                //     </Box>
+                // ),
+                title: 'Login Failed',
             })
             return false
 
