73ab77c8c9588dd84869804c592050350daa46d9
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-18 13:18:44 +0530

    Added Data

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Button } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { DASHBOARD } from '../../lib/routes'
+
 
 const Navbar = () => {
   return (
     <Flex shadow='sm' pos='fixed' width='full' borderTop='6px solid' height='16' zIndex='3' justify='center' bg="white" borderBottomRadius='6'>
-        <Flex px='4' w='full' align='center' maxW='1200px' bg='white'>
+        <Flex px='4' w='full' align='center' maxW='1200px' bg='red'>
+            <h1>Home</h1>
 
+            <button className>
+              Log Out
+            </button>
         </Flex>
     </Flex>
   )
